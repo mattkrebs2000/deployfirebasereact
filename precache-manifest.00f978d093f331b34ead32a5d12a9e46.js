1bab542027b22000efe5427d96f2a49e31542638
Matthew Krebs <matthewkrebs@Matthews-iMac.home>
2021-02-11 20:24:11 -0500

    Updates

---
+++ precache-manifest.00f978d093f331b34ead32a5d12a9e46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "707c87f60f8639558eef",
+    "url": "/deployfirebasereact/static/css/main.b41b8942.chunk.css"
+  },
+  {
+    "revision": "707c87f60f8639558eef",
+    "url": "/deployfirebasereact/static/js/main.707c87f6.chunk.js"
+  },
+  {
+    "revision": "4ca57366c8ded9556c11",
+    "url": "/deployfirebasereact/static/js/1.4ca57366.chunk.js"
+  },
+  {
+    "revision": "3c1cb5e46c2d43437744",
+    "url": "/deployfirebasereact/static/js/runtime~main.3c1cb5e4.js"
+  },
+  {
+    "revision": "12b24da1ae5bbf74b292a5fdb6557d51",
+    "url": "/deployfirebasereact/index.html"
+  }
+];
